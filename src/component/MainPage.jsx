5f3f0bdfec890996c8ff62d8ce5f9f4c326e42c2
unknown <champman190@gmail.com>
2024-10-16 12:36:21 -0700

    second commit

--- src/component/MainPage.jsx
+++ src/component/MainPage.jsx
@@ -4,7 +4,7 @@ import malhubWorkSpace from "../assets/malhubWorkSpace.jpg";
 
 function MainPage() {
   return (
-    <div className="flex gap-20">
+    <div className="flex gap-20 text-blue-200">
       <div>
         <img
           className="w-[700px] h-[400px] rounded-[500px] ml-5 mt-5"
@@ -15,9 +15,9 @@ function MainPage() {
       <div className="mt-5">
         <i className="font-semibold">
           Makers and Leaders Hub <span>(MALhub) </span> <br />
-          is an open, serene and beautiful shared workspace <br /> for startups and
-          entrepreneurs at a lower cost than they <br /> would spend on space of their
-          own. 
+          is an open, serene and beautiful shared workspace <br /> for startups
+          and entrepreneurs at a lower cost than they <br /> would spend on
+          space of their own.
         </i>
         <img
           className="w-[300px] h-[100px] mt-[150px] ml-[150px]"
